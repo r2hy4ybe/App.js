40a091397026d801d24bba1c7431d2dd8fa9f934
r2hy4ybe <78624421+r2hy4ybe@users.noreply.github.com>
2023-03-09 17:12:33 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div
+      className="App"
+      style={{
+        backgroundImage: `url("https://1b54gc.csb.app/src/Snslp3.png")`
+      }}
+    >
+      <h1>Hello Seasons!</h1>
+      Wanna see pretty seasons pics from (US) and around the world?
+      <button>Click Here</button>
+    </div>
+  );
+}
